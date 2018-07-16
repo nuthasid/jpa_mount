6412e3669af43015ce98663279f218be149c9aaa
nuthasid <nuthasid.r@outlook.com>
2018-07-16 10:18:30 +0700

    show progress while loading file

--- src/dataimporter.py
+++ src/dataimporter.py
@@ -3,12 +3,17 @@ def dataImporter(filename):
 
     import json
     import pandas as pd
+    import sys
     try :
         datamat = pd.DataFrame(columns=["title", "desc", "tag"])
         with open(filename, 'r', encoding='utf-8') as fin:
+            counter = 1
             for line in fin:
                 line_dict = json.loads(line, encoding='utf-8')
                 datamat = datamat.append(line_dict, ignore_index=True)
+                print('Loading doc: ' + str(counter), end='\r')
+                sys.stdout.flush()
+            print('')
     except:
         raise
     return datamat
