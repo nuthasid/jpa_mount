2dbbc1d66010446f24bed173a0b6cd37cbcfc0c7
nuthasid <nuthasid.r@outlook.com>
2018-07-16 14:06:20 +0700

    add progress bar

--- src/dataimporter.py
+++ src/dataimporter.py
@@ -3,17 +3,22 @@ def dataImporter(filename):
 
     import json
     import pandas as pd
-    import sys
-    try :
+    from tqdm import tqdm
+    # import sys
+    try:
         datamat = pd.DataFrame(columns=["title", "desc", "tag"])
         with open(filename, 'r', encoding='utf-8') as fin:
-            counter = 1
-            for line in fin:
+            # counter = 0
+            lines = fin.readlines()
+            pbar = tqdm(total=len(lines))
+            for line in lines:
                 line_dict = json.loads(line, encoding='utf-8')
                 datamat = datamat.append(line_dict, ignore_index=True)
-                print('Loading doc: ' + str(counter), end='\r')
-                sys.stdout.flush()
-            print('')
+                pbar.update()
+                # if counter % 1000 == 0:
+                    # print('Loading doc: ' + str(counter), end='\r')
+                    # sys.stdout.flush()
+            pbar.close()
     except:
         raise
     return datamat
